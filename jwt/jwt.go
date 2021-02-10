74b611d931218a7874af1f4a1e850e48d1414670
YasserCR <yassros16@gmail.com>
2021-02-09 19:55:51 -0600

    Endpoint ver perfil

---
+++ jwt/jwt.go
@@ -0,0 +1,30 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/YasserCR/clontter/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func GeneroJWT(t models.Usuario) (string, error) {
+	miClave := []byte("ClaveYasser")
+
+	payload := jwt.MapClaims{
+		"email":            t.Email,
+		"nombre":           t.Nombre,
+		"apellidos":        t.Apellidos,
+		"fecha_nacimiento": t.FechaNacimiento,
+		"biografia":        t.Biografia,
+		"ubicacoion":       t.Ubicacion,
+		"sitioweb":         t.SitioWeb,
+		"_id":              t.ID.Hex(),
+		"exp":              time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenStr, err := token.SignedString(miClave)
+	if err != nil {
+		return tokenStr, err
+	}
+	return tokenStr, nil
+}
